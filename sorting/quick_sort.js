a19d1eb1c3298c2030ac721fc943a857c8289a07
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2026-01-04 19:02:43 +0500

    Сортировки

--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -3,10 +3,10 @@
  * Space complexity O(log n)
  */
 
-const array = [ 0, 3, 4, 2, -1, 3, 6, 7, 13, -4, 45, 22, 53, 16 ];
+const array = [0, 3, 4, 2, -1, 3, 6, 7, 13, -4, 45, 22, 53, 16];
 
 function quickSort(array) {
-    if(array.length <= 1) {
+    if (array.length <= 1) {
         return array;
     }
 
@@ -14,17 +14,52 @@ function quickSort(array) {
     let pivot = array[pivotIndex];
     let less = [];
     let greater = [];
-    for(let i = 0; i < array.length; i++) {
-        if(i === pivotIndex) {
+    for (let i = 0; i < array.length; i++) {
+        if (i === pivotIndex) {
             continue;
-        } else if(array[i] < pivot) {
+        } else if (array[i] < pivot) {
             less.push(array[i]);
         } else {
             greater.push(array[i]);
         }
     }
 
-    return [ ...quickSort(less), pivot, ...quickSort(greater) ];
+    return [...quickSort(less), pivot, ...quickSort(greater)];
 }
 
 console.log(quickSort(array));
+
+
+function partition(array, left, right) {
+    let pivotIndex = Math.floor((left + right) / 2);
+    const pivot = array[pivotIndex];
+    let i = left - 1;
+    let j = right + 1;
+
+    while (true) {
+        do {
+            i++;
+        } while (array[i] < pivot);
+
+        do {
+            j--;
+        } while (array[j] > pivot);
+
+        if (i > j) return j;
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+/**
+ * С двумя указателями, без создания массиов
+ */
+function quickSort2(array, left = 0, right = array.length - 1) {
+    if (left >= right) {
+        return;
+    }
+
+    const pivotIndex = partition(array, left, right);
+    quickSort2(array, left, pivotIndex);
+    quickSort(array, pivotIndex + 1, right);
+}
